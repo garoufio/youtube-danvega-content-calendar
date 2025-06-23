6c7a5e8d1e976b4149dc6cf7797d372c962faf38
Panagiotis Triantafyllos <triantafyllospg@dias.com.gr>
2025-06-23 10:09:07 +0300

    project initialization

---
+++ src/main/java/com/youtube/danvega/content_calendar/Application.java
@@ -0,0 +1,17 @@
+package com.youtube.danvega.content_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+	}
+
+}
