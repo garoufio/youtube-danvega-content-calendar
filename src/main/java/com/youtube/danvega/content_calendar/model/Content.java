39a7713ab667b743540a32059526d5bd57b0b96c
Panagiotis Triantafyllos <triantafyllospg@dias.com.gr>
2026-01-30 17:28:37 +0200

    use @Table annotation to declare the table name in the entity class

--- src/main/java/com/youtube/danvega/content_calendar/model/Content.java
+++ src/main/java/com/youtube/danvega/content_calendar/model/Content.java
@@ -3,9 +3,11 @@ package com.youtube.danvega.content_calendar.model;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
+@Table(value = "Content")   // not necessary if class name matches table name
 public record Content(
     @Id
     Integer id,
